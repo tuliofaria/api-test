357b6354bba2d1e394c0426c6056801c61807ef7
Tulio Faria <tuliofaria@gmail.com>
2021-04-22 21:56:11 -0300

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Server is running...')
+})
+
+app.get('/users', (req, res) => {
+  res.send([{ id: 1, name: 'Tulio' }])
+})
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log('Server is not running.')
+  } else {
+    console.log('server is running.')
+  }
+})
